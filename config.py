e53093ac1c21042e0767d75a0c9556fa92d2eb2a
HEM45 <mikimaos23@outlook.com>
2025-04-26 02:07:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "7571130670:AAGZsaxxoB9EOG1kvRZRuqrpIpHQ5NVPNN8"  # Your telegram bot token
+DEVELOPER_ID = 2007860433  # Your telegram id (used for error reporting and private mode)
+IS_BOT_PRIVATE = False  # Change this to True to make bot private
